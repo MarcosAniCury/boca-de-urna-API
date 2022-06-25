6193758f8275b4217d1286f64d7eb2dc71fae247
MarcosAniCury <curymarcos@live.com>
2022-06-25 20:32:53 -0300

    modify controllers

--- boca-de-urna-API/app/Models/President.php
+++ boca-de-urna-API/app/Models/President.php
@@ -7,18 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class President extends Model
 {
-    //Table name
     protected $table = "presidents";
-
-    //Primary key collumn
-    protected $primaryKey = "id";
-
-    //Database Connection
-    protected $connection = 'mysql';
-
-    //Atributtes
-    protected $attributes = [
-        'name' => null,
-        'code' => null,
-    ];
 }
