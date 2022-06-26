318037886245fd3f60ad2eaa58505a62f6b704db
MarcosAniCury <curymarcos@live.com>
2022-06-26 00:20:28 -0300

    how add new route getUserInfo

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\userController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('teste', function () { return ['teste' => 'teste']; });
+
+Route::post('getUserInfo', 'userController@getUserInfo');
