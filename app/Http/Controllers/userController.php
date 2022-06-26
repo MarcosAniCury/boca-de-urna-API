7bcc686704b505dab9688c397faa3f03d6f56ad8
MarcosAniCury <curymarcos@live.com>
2022-06-26 00:20:03 -0300

    add user controller

---
+++ app/Http/Controllers/userController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class userController extends Controller
+{
+    public function getUserInfo($request)
+    {
+        $response = Http::post('https://apigateway.conectagov.estaleiro.serpro.gov.br/api-cpf-light/v2/consulta/cpf', [
+            'x-cpf-usuario' => $request->cpf
+        ]);
+
+        return $response;
+    }
+}
