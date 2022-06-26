780c5818f3f4c1ac1889632f6cc7599a8c3dde70
MarcosAniCury <curymarcos@live.com>
2022-06-25 23:10:31 -0300

    heroku test

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('teste', function () { return view('welcome.blade.php'); });
+Route::get('teste', function () { return ['teste' => 'teste']; });
